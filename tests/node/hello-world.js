0cd84e0050d518d6ea441e5b6ed228908b7ab61e
Terje Christensen <terchris@users.noreply.github.com>
2024-09-18 14:29:12 +0200

    build test after latest version of runner

--- tests/node/hello-world.js
+++ tests/node/hello-world.js
@@ -3,5 +3,5 @@
 // Purpose: A simple Node.js program to test the GitHub Actions runner.
 // This program prints "Hello, World!" and the current date and time.
 
-console.log('Hello, World! Build it');
+console.log('Hello, World! JALLA');
 console.log('Current date and time:', new Date().toISOString());
